8b17ec335b7fa8c31e59242e87c59f2c275492f1
turt1e18 <pk0820@cu.ac.kr>
2026-04-07 21:12:03 +0900

    필요없는 import 코드 정리

--- src/main/kotlin/com/turt1e18/rwhiskey/rwhiskey/api/auth/controller/AuthController.kt
+++ src/main/kotlin/com/turt1e18/rwhiskey/rwhiskey/api/auth/controller/AuthController.kt
@@ -6,7 +6,6 @@ import com.turt1e18.rwhiskey.rwhiskey.api.auth.service.AuthService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
